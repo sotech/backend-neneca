894d1f8407e520949cea85e10976164e9879ae3c
German Sommariva <germansommariva@gmail.com>
2021-11-25 12:34:17 -0300

    Feature - Express

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 require('./src/utils/envConfig')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const port = process.env.PORT || 8080;
+//Rutas
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en ` + port);
+});
