dd658f9de7abadf636ff1e7b033e3d4db8e4d874
German Sommariva <germansommariva@gmail.com>
2021-12-19 03:32:10 -0300

    Ajustes al middleware en las rutas

--- src/routes/ordenRoutes.js
+++ src/routes/ordenRoutes.js
@@ -4,7 +4,7 @@ const { verificarToken } = require('../controllers/usuarioController');
 const ordenController = require("../controllers/ordenController.js");
 
 router.get('/obtener', verificarToken, ordenController.obtenerOrdenes);
-router.post('/agregar', verificarToken, ordenController.agregarOrden);
+router.post('/agregar', ordenController.agregarOrden);
 router.put('/actualizar/:id', verificarToken, ordenController.actualizarOrden);
 router.delete('/eliminar/:id', verificarToken, ordenController.eliminarOrden);
 
