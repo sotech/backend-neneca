4d35d4c8ed9278585c7f032da8967539ced99d8a
German Sommariva <germansommariva@gmail.com>
2021-12-21 13:48:50 -0300

    Feature - Seguimiento orden

--- src/routes/ordenRoutes.js
+++ src/routes/ordenRoutes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const { verificarToken } = require('../controllers/usuarioController');
 const ordenController = require("../controllers/ordenController.js");
 
+router.get('/obtener/:id', ordenController.obtenerOrden);
 router.get('/obtener', verificarToken, ordenController.obtenerOrdenes);
 router.post('/agregar', ordenController.agregarOrden);
 router.put('/actualizar/:id', verificarToken, ordenController.actualizarOrden);
