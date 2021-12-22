084d1306c04f5645b4a654bae9fac90bd8db3cd4
German Sommariva <germansommariva@gmail.com>
2021-12-21 21:29:20 -0300

    Feature - Main and 404

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 8080;
 const productoRoutes = require('./src/routes/productoRoutes')
 const ordenRoutes = require('./src/routes/ordenRoutes')
 const usuarioRoutes = require('./src/routes/usuarioRoutes')
+const mainRoutes = require('./src/routes/mainRoutes')
 
 app.use(express.json());
 app.use(morgan('dev'));
@@ -15,6 +16,8 @@ app.use(morgan('dev'));
 app.use('/api/v1/producto', productoRoutes)
 app.use('/api/v1/orden', ordenRoutes)
 app.use('/api/v1/usuario', usuarioRoutes)
+app.get('/', mainRoutes.mainPage)
+app.use(mainRoutes.get404)
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en ` + port);
