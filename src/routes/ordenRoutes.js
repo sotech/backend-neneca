3fe951bef05fb1ee63ce9738ec560b30c6ce493c
German Sommariva <germansommariva@gmail.com>
2022-02-24 10:21:06 -0300

    Fix - Stock

--- src/routes/ordenRoutes.js
+++ src/routes/ordenRoutes.js
@@ -7,6 +7,6 @@ router.get('/obtener/:id', ordenController.obtenerOrden);
 router.get('/obtener', verificarToken, ordenController.obtenerOrdenes);
 router.post('/agregar', ordenController.agregarOrden);
 router.put('/actualizar/:id', verificarToken, ordenController.actualizarOrden);
-router.delete('/eliminar/:id', verificarToken, ordenController.eliminarOrden);
+router.delete('/eliminar/:id', /*verificarToken,*/ ordenController.eliminarOrden);
 
 module.exports = router;
